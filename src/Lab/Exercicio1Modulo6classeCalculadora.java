9f22663d20f3ce5d98a27b4447ac1b8fbb5af04d
Morais <matheus.morais@capgemini.com>
2019-11-04 16:30:57 -0300

    last commit 04/11

---
+++ src/Lab/Exercicio1Modulo6classeCalculadora.java
@@ -0,0 +1,41 @@
+package Lab;
+
+public class Exercicio1Modulo6classeCalculadora {
+
+	//SUBTRAIR
+	public double subtrair(double a, double b) {		
+		return a-b;
+	}
+	
+	public int subtrair(int a, int b) {		
+		return a-b;
+	}
+	
+	//MULTIPLICAR
+	public int multiplicar(int a, int b) {
+		return a*b;
+	}
+	
+	public double multiplicar(double a, double b) {
+		return a*b;
+	}
+	
+	//DIVIDIR
+	public int dividir(int a, int b) {
+		return a/b;
+	}
+	
+	public double dividir(double a, double b) {
+		return a/b;
+	}
+	
+	//SOMA
+	public int soma(int a, int b) {
+		return a+b;
+	}
+	
+	public double soma(double a, double b) {
+		return a+b;
+	}
+	
+}
